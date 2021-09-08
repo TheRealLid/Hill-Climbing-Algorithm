ae9cba16671827567b6ba985a86ffa79f6587b08
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2021-09-07 18:44:00 -0700

    bug fix

--- hill.py
+++ hill.py
@@ -6,9 +6,10 @@ current.show()
 
 i_index = 0
 j_index = 0
+temp1 = copy.deepcopy(current)
 for i in range(0, 5):
     if current.map[0][i] == 1:
-        current.map[0][i] = 0
+        temp1.map[0][i] = 0
         j_index = i
         break
 arr = []
@@ -47,53 +48,56 @@ if i_down and j_left:
     ij_downLeft = True
 
 for i in range(0, 8):
-    temp = copy.deepcopy(current)
+    temp = copy.deepcopy(temp1)
     if ij_upLeft and temp.map[i_index - 1][j_index - 1] != 1:
         temp.map[i_index - 1][j_index - 1] = 1
         arr.append(temp)
         ij_upLeft = False
-        print("upLeft")
+        print("upLeft" + " " + str(i))
 
     elif ij_upRight and temp.map[i_index - 1][j_index + 1] != 1:
         temp.map[i_index - 1][j_index + 1] = 1
         arr.append(temp)
         ij_upRight = False
-        print("upRight")
+        print("upRight"  + " " + str(i))
 
     elif ij_downLeft and temp.map[i_index + 1][j_index - 1] != 1:
         temp.map[i_index + 1][j_index - 1] = 1
         arr.append(temp)
         ij_downLeft = False
-        print("downLeft")
+        print("downLeft"  + " " + str(i))
 
     elif ij_downRight and temp.map[i_index + 1][j_index + 1] != 1:
         temp.map[i_index + 1][j_index + 1] = 1
         arr.append(temp)
         ij_downRight = False
-        print("downRight")
+        print("downRight" + " " + str(i))
 
     elif i_up and temp.map[i_index-1][j_index] != 1:
         temp.map[i_index-1][j_index] = 1
         arr.append(temp)
         i_up = False
-        print("up")
+        print("up" + " " + str(i))
     elif i_down and temp.map[i_index + 1][j_index] != 1:
         temp.map[i_index + 1][j_index] = 1
         arr.append(temp)
         i_down = False
-        print("down")
+        print("down" + " " + str(i))
     elif j_left and temp.map[i_index][j_index - 1] != 1:
         temp.map[i_index][j_index - 1] = 1
         arr.append(temp)
         j_left = False
-        print("left")
+        print("left" + " " + str(i))
     elif j_right and temp.map[i_index][j_index + 1] != 1:
         temp.map[i_index][j_index + 1] = 1
         arr.append(temp)
         j_right = False
-        print("right")
+        print("right" + " " + str(i))
 print("length of arr = " + str(len(arr)))
 for i in range(0, len(arr)):
+    arr[i].fit = 0
     arr[i].fitness()
     arr[i].show()
+print("original")
+current.show()
 
